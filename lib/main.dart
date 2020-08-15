f3676857dea1223cfd2275c61b4c4c8413cea9d5
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-08-16 02:47:34 +0900

    rm comment

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Interaction Playground',
         theme: ThemeData(
           backgroundColor: Colors.white,
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-          // This makes the visual density adapt to the platform that you run
-          // the app on. For desktop platforms, the controls will be smaller and
-          // closer together (more dense) than on mobile platforms.
         ),
         home: Scaffold(body: ListPage()));
   }
@@ -54,8 +40,7 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
         body: Center(
             child: GestureDetector(
-      onTap: () => Navigator.of(context)
-          .pop(),
+      onTap: () => Navigator.of(context).pop(),
       child: Hero(
         tag: 'hero_container_$value',
         child: Container(
